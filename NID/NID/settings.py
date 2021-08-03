c80344402fdff9807abe6efd013593229793d439
SubsanMainali <subashmainali447@gmail.com>
2021-08-04 01:46:41 +0545

    Old indexes removed from .git; indexing started with .gitignore rule

--- NID/NID/settings.py
+++ NID/NID/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'address',
-    'django_extensions',
+    #'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
