f35b1753de1eee3da942b600a20031b9ef843ad3
SubsanMainali <subashmainali447@gmail.com>
2021-08-06 23:10:31 +0545

    Modification in home/templates and home/static; will comeback later.

--- NID/home/views.py
+++ NID/home/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, "home/index.html", {
         "title":"Home",
-        "message": "Welcome to the landing page"
+        "message": "Welcome!"
     })
