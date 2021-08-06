babae140fe9877e5a1aa6ebebf7d4e9c566e0673
Prashant Shrestha <075bei024.prashant@pcampus.edu.np>
2021-08-06 23:15:06 +0545

    Edit Citizenship and License models

--- NID/documents/models.py
+++ NID/documents/models.py
@@ -2,38 +2,45 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 from address.models import *
 
-# Create your models here.
-# class BloodGroup(models.Model):
-#     id = models.IntegerField(primary_key=True)
-#     blood_group = models.CharField(max_length=3)
+#Create your models here.
+# issue with documents: for new creation documents id should be assigned not entered; solution: autofield for primary key, char field for document_no
+# issue with license: 
+class BloodGroup(models.Model):
+    id = models.IntegerField(primary_key=True)
+    blood_group = models.CharField(max_length=3)
 
-#     def __str__(self):
-#         return f'Group({self.blood_group})'
+    def __str__(self):
+        return f'Group({self.blood_group})'
     
 
-# class DrivingLicenseIssueCentre(models.Model):
-#     centre_id = models.IntegerField(primary_key=True)
-#     province_id =""
-#     district_id =""
-#     local_id = ""
-#     name = models.CharField(max_length=65, null=False)
+class DrivingLicenseIssueCentre(models.Model):
+    centre_id = models.IntegerField(primary_key=True)
+    province = models.ForeignKey(Region, on_delete=models.PROTECT, related_name="license_issue_centers")
+    district = models.ForeignKey(District, on_delete=models.PROTECT, related_name="license_issue_centers")
+    local = models.ForeignKey(LocalBody, on_delete=models.PROTECT, related_name="license_issue_centers") 
+    name = models.CharField(max_length=65, blank=False, null=False)
 
-#     def __str__(self):
-#         return f"Licence Issued from {self.name}"
+    def __str__(self):
+        return f"Licence Issued from {self.name}"
 
-# class LicenceCategory(models.Model):
-#         id = models.IntegerField(primary_key=True, null=False)
-#         symbol = models.CharField(max_length=2, null=False)
-#         description = models.CharField(max_length=50, null=False)
+class DrivingLicenseCategory(models.Model):
+    id = models.IntegerField(primary_key=True, null=False)
+    symbol = models.CharField(max_length=2, null=False)
+    description = models.CharField(max_length=50, null=False)
 
-#         def __str__(self):
-#             return f"{self.symbol} : {self.description}"
+    def __str__(self):
+        return f"{self.symbol} : {self.description}"
 
-# class DrivingLicense(models.Model):
-#     id = models.IntegerField(primary_key=True)
-#     issue_date = models.DateField(auto_now=False, auto_now_add=False)
-#     issue_centre = models.ForeignKey(DrivingLicenseIssueCentre, on_delte=models.PROTECT)
-#     license_type = ""
+class DrivingLicense(models.Model):
+    id = models.IntegerField(primary_key=True)
+    issue_date = models.DateField(auto_now=False, auto_now_add=False)
+    issue_centre = models.ForeignKey(DrivingLicenseIssueCentre, on_delete=models.PROTECT, related_name="licenses")
+    #many to many
+    #license_category = models.ForeignKey(DrivingLicenseCategory, on_delete=models.PROTECT, related_name="licenses")  
+    document_photo = models.FileField(upload_to="media/", blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.id}"
 
 # class DocumentID(models.Model):
 #     pass
@@ -59,11 +66,12 @@ class Citizenship(models.Model):
         ('F', 'Female'),
         ('O', 'Others')
     )
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
+    citizenship_no = models.CharField(max_length=64, blank=False, null=False)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     #approval = models.ForeignKey(Approval, on_delete=models.PROTECT, null=True)
-    #photo_front = models.FileField()
-    #photo_back = models.FileField()
+    photo_front = models.FileField(upload_to="media/", blank=True, null=True)
+    photo_back = models.FileField(upload_to="media/", blank=True, null=True)
     first_name = models.CharField(max_length=32, blank=False)
     middle_name = models.CharField(max_length=32, blank=True, null=True)
     last_name = models.CharField(max_length=32, blank=False)
@@ -104,7 +112,7 @@ class Citizenship(models.Model):
     spouse_citizenship_id = models.IntegerField(blank=True, null=True)
 
     citizenship_type = models.ForeignKey(CitizenshipType, on_delete=models.PROTECT, related_name="citizenships")
-    #face_photo = models.FileField()
+    face_photo = models.FileField(upload_to="media/")
 
     issue_date_bs = models.DateField()
     citizenship_act = models.ForeignKey(CitizenshipAct, on_delete=models.PROTECT, related_name="citizenships")
