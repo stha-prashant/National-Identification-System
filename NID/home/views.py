a8cdde277a6c3131af5a7cabba1bf27f33d9ef1d
SubsanMainali <subashmainali447@gmail.com>
2021-08-17 00:14:00 +0545

    project icon added

--- NID/home/views.py
+++ NID/home/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "home/index.html", {
-        "title":"Home"
+        'title':'Home'
     })
